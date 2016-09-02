71392d1410b4dd4bda0ca6a929b0c4eefad2fded
hk_core <hk.core9092@gmail.com>
2016-09-02 15:29:20 +0800

    添加cors

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,4 +1,14 @@
 module.exports = function (app) {
+    //CORS
+    app.use('/', function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    });
+    app.options('/*', function (req, res) {
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.end();
+    });
+
     //不需要token
     app.use('/', require('./free'));
 
